63637bc72100d1cd84482d8b25c519011fedd376
TikaBennekum <107108939+TikaBennekum@users.noreply.github.com>
2025-02-24 20:19:37 +0100

    monte carlo setup done! :)

--- src/scientificcomputing_set2/monte_carlo.py
+++ src/scientificcomputing_set2/monte_carlo.py
@@ -5,15 +5,25 @@ Student IDs: 15746704, 15719227 and 13392425
 
 File description:
     Monte Carlo implementation of the DLA, using random walkers.
+    A cell has the value 0 when empty, 1 when containing the cluster and 2 when containing a walker.
 """
 
 import numpy as np
+
+global WIDTH
+global SPACE
+
 
 def initialize_grid(seed, width):
     """
     Initialize grid given a width as parameter. It assumes a square grid.
     A randomly selected cell on the bottom row is the starting cluster.
     """
+    global WIDTH
+    global SPACE
+    WIDTH = width
+    SPACE = True
+
     # Set empty grid
     grid = np.zeros((width, width))
 
@@ -29,59 +39,110 @@ def add_walker(seed, grid):
     """
     Adds a Random Walker to the grid on the top boundary.
     """
-    np.random.seed(seed)
-    random_col = np.random.randint(0, len(grid))
-    if grid[0, random_col] != 1:
+    global SPACE
+    empty_cells = np.where(grid[0] == 0)[0]
+
+    if empty_cells.size > 0:
+        random_col = np.random.choice(empty_cells)
         grid[0, random_col] = 2
+    else:
+        SPACE = False
+        raise ValueError("No new walkers can be added to the system!")
 
     return grid
+
+
+def check_neighbourhood(grid, i_n, j_n):
+    """
+    Checks wheter the random walker is in the neightbourhood of the cluster.
+    """
+    if i_n + 1 < WIDTH and grid[i_n + 1, j_n] == 1:
+        return True
+    if i_n - 1 >= 0 and grid[i_n - 1, j_n] == 1:
+        return True
+    if j_n + 1 < WIDTH and grid[i_n, j_n + 1] == 1:
+        return True
+    if j_n - 1 >= 0 and grid[i_n, j_n - 1] == 1:
+        return True
+
+    return False
+
+
+def new_indexes(temp_grid, i, j):
+    """
+    Computes new indexes for random walker.
+
+    Option1:
+        Random walker goes up or down, if it goes out of bounds,
+        it is removed and a new random walker is created.
+    Option2:
+        Random walker goes left or right, if it goes out of bounds,
+        it reapears on the other side of the grid.
+    """
+    direction = np.random.choice(["up", "down", "left", "right"])
+    i_n, j_n = i, j
+
+    if direction == "up":
+        if i > 0:
+            i_n -= 1
+        else:
+            temp_grid[i, j] = 0
+            if SPACE:
+                temp_grid = add_walker(temp_grid)
+    if direction == "down":
+        if i < WIDTH - 1:
+            i_n += 1
+        else:
+            temp_grid[i, j] = 0
+            if SPACE:
+                temp_grid = add_walker(temp_grid)
+    elif direction == "left":
+        if j > 0:
+            j_n -= 1
+        else:
+            j_n = WIDTH - 1
+    elif direction == "right":
+        if j < WIDTH - 1:
+            j_n += 1
+        else:
+            j_n = 0
+
+    # Check if the new position is occupied by another walker
+    if temp_grid[i_n, j_n] == 2:
+        # Prevent the walker from moving into occupied cell
+        return temp_grid, i, j
+
+    return (temp_grid, i_n, j_n)
+
 
 def update_grid(seed, grid):
     """
     Updates locations for all the random walkers
     and checks wheter the cluster is in their neighbourhood.
     """
-    np.random.random(seed)
-    copy_grid = grid.copy()
+    np.random.seed(seed)
+    temp_grid = grid.copy()
 
-    for i in grid:
-        for j in grid:
-            # randomly choose wheter i + 1, i - 1, j + 1 or j - 1
-            if grid[i, j] == 2:  # Check if the cel containts a walker
-                direction = np.random.choice(["up", "down", "left", "right"])
+    for i in range(WIDTH):
+        for j in range(WIDTH):
+            if grid[i, j] == 2:  # Check if the cel contains a walker
+                temp_grid, i_n, j_n = new_indexes(temp_grid, i, j)
+                cluster = check_neighbourhood(temp_grid, i_n, j_n)
 
-                # Compute new position
-                i_n, j_n = i, j
-                # Random walker goes up or down, if it goes out of bounds,
-                # it is removed and a new random walker is created.
-                if direction == "up":
-                    if i > 0:
-                        i_n -= 1
-                    else:
-                        grid[i, j] = 0
-                        grid = add_walker(grid)
-                if direction == "down":
-                    if i < 0:
-                        i_n += 1
-                    else:
-                        grid[i, j] = 0
-                        grid = add_walker(grid)
-                # Random walker goes left or right, if it goes out of bounds,
-                # it reapears on the other side of the grid.
-                elif direction == "left":
-                    if j > 0:
-                        j_n -= 1
-                    else:
-                        j_n = len(grid) - 1
-                elif direction == "right":
-                    if j < len(grid) - 1:
-                        j_n += 1
-                    else:
-                        j_n = 0
+                temp_grid[i, j] = 0
+                # If there is a cluster in the new cel, it will become part of the
+                # cluster, else the walker will move to the new spot.
+                if cluster:
+                    temp_grid[i_n, j_n] = 1
+                else:
+                    temp_grid[i_n, j_n] = 2
 
-                # Move the walker to the new position
-                copy_grid[i, j] = 0  # Remove from old position
-                copy_grid[i_n, j_n] = 1  # Place at new position
+    grid[:] = temp_grid
+    return grid
 
 
+def start_simulation(seed, steps, width):
+    grid = initialize_grid(seed, width)
 
+    for _ in range(steps):
+        grid = update_grid(seed, grid)
