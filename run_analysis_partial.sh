58ecda3ae7b5a9fe2b9d1e001306b61d4a43742c
PetrChalupsky <petr.chalupsky@student.uva.nl>
2025-03-03 22:20:51 +0100

    added poetry run to also run_analysis_partial.sh

--- run_analysis_partial.sh
+++ run_analysis_partial.sh
@@ -2,19 +2,19 @@
 
 echo "Running analysis without long runs"
 echo "Running calculate_gray_scott"
-python scripts/calculate_gray_scott.py
+poetry run python scripts/calculate_gray_scott.py
 echo "Running calculate_monte_carlo"
-python scripts/calculate_monte_carlo.py
+poetry run python scripts/calculate_monte_carlo.py
 echo "Running calculate_DLA_Laplace"
-python scripts/calculate_DLA_Laplace.py
+poetry run python scripts/calculate_DLA_Laplace.py
 
 echo "Creating plots"
 echo "Running visualize_gray_scott"
-python scripts/visualize_gray_scott.py
+poetry run python scripts/visualize_gray_scott.py
 echo "Running visualize_monte_carlo"
-python scripts/visualize_monte_carlo.py
+poetry run python scripts/visualize_monte_carlo.py
 echo "Running visualize_DLA_Laplace"
-python scripts/visualize_DLA_Laplace.py
+poetry run python scripts/visualize_DLA_Laplace.py
 
 echo "Analysis finished, hooman!"
 
